8b4b38d638fc4229081da6d26e23d75f8df27d6d
azizasyd <azizasydykova0202@gmail.com>
2024-10-30 00:26:59 +0600

    finish two pointer problems

--- leetcode_75/two_pointers/move_zeroes.cpp
+++ leetcode_75/two_pointers/move_zeroes.cpp
@@ -3,7 +3,7 @@
 class Solution {
 public:
     void moveZeroes(std::vector<int>& nums) {
-        int left = 0;
+        int left = 0; // for zeroes
         for(int right = 0; right < nums.size(); ++right){
             if(nums[right] != 0){
                 std::swap(nums[left], nums[right]);
